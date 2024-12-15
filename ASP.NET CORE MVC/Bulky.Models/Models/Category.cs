db803ac7cd5f90fd0deed6bda83fc5899d544ac4
Sivakumar Sathiyamoorthy <ssivacse@gmail.com>
2024-12-15 13:25:02 +0530

    Add files via upload

---
+++ ASP.NET CORE MVC/Bulky.Models/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20,ErrorMessage ="Name should <=20 in length")]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }=string.Empty;
+        
+        [DisplayName("Category Order")]
+        [Range(1,100,ErrorMessage="Order should be in 1 to 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
